abd382698f0382c5afe25d7706b8aab7612808f8
Menkveld <menke.veerman@gmail.com>
2022-12-09 15:59:33 +0100

    update version nr

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="PIDPATH",
-    version='1.0.0',
+    version='1.0.1',
     description='M8C T5 PIDPATH PACKAGE',
     packages=['PIDPATH'],
     install_requires=[
