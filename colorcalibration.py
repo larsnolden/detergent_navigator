92e5de1b75740b99184cbfa2a0f7434033290e07
julian <45972321+BoilingFusion@users.noreply.github.com>
2023-01-11 16:29:18 +0100

    Update colorcalibration.py

--- colorcalibration.py
+++ colorcalibration.py
@@ -1,12 +1,12 @@
+# Guid:
+# press (left mouse button) to add a pixel to the bound
+# press (z) to remove the previously added pixel from the bound
+# press (q) to quit
+
 import cv2
 import numpy as np
 
 imcap = cv2.VideoCapture(0)
-
-mid = np.array([105+10, 202+10, 130+10])
-e = np.array([80, 35, 40])
-low = np.array([36, 25, 25])
-high = np.array([86, 255, 255])
 
 mouse_x = 0
 mouse_y = 0
@@ -23,46 +23,45 @@ def onMouse(event, x, y, _flags, _param):
 cv2.namedWindow("capture")
 cv2.setMouseCallback("capture", onMouse)
 
-bs = [None]
+hist = []
+bound = None
 
 while True:
     ok, img = imcap.read()
-    img = cv2.GaussianBlur(img, (5, 5), 0)
     if not ok:
         break
-    
     height, width, depth = img.shape
-
     
+    img = cv2.GaussianBlur(img, (5, 5), 0)
     img = cv2.circle(img, (mouse_x, mouse_y), 10, (10,10,255))
     
     clr = img[mouse_y][mouse_x]
-    if bs and clicked:
-        if bs[-1] == None:
-            bs[-1] = (clr, clr)
+    if clicked:
+        if bound == None:
+            bound = (clr, clr)
         else:
-            max, min = bs[-1]
+            hist.append(bound)
+            max, min = bound
             max = np.maximum(max, clr)
             min = np.minimum(min, clr)
-            bs[-1] = (max, min)
+            bound = (max, min)
         clicked = False
 
-    masks = []
-    for b in bs:
-        if b == None:
-            continue
-        masks.append(cv2.bitwise_not(cv2.inRange(img, b[1], b[0])))
-    for mask in masks:
+    
+    if bound != None:
+        mask = cv2.bitwise_not(cv2.inRange(img, bound[1], bound[0]))
         img = cv2.bitwise_and(img, img, mask=mask)
     cv2.imshow("capture", img)
     key = cv2.waitKey(1)
-    print(bs)
+    if bound != None:
+        print(f"({bound[1].tolist()}, {bound[0].tolist()})")
     if key == ord('q'):
         break
-    if key == ord('n'):
-        bs.append(None)
     if key == ord('z'):
-        del bs[-1]
+        if hist:
+            bound = hist.pop(-1)
+        else:
+            bound = None
 
 imcap.release()
 cv2.destroyAllWindows()
